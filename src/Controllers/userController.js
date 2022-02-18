6b9e209b4763b9caf202b1de6fdb7fc307bc7aeb
viki bril <bril2010@gmail.com>
2022-02-18 18:16:05 +0200

    newJwt

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,36 +1,56 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken')
-const axios = require('axios');
-const userData = require('../Model/user');
-const path = require('path');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const userData = require("../Model/user");
+const path = require("path");
 
-const loginUser = async(req, res) => {
-    const userId = req.query.userId;
-    const password = req.query.password;
-    userData.findOne({ userId: userId, password:password}).then((user) =>{
-        res.setHeader("Content-Type", "text/plain");
-        if(!user){
-            return res.status(404).send("login failed");
-        }
-        const accessToken=jwt.sign(userId,process.env.ACCESS_TOKEN_SECRET);
-        res.json({accessToken: accessToken})
-        return res.status(200).send(user);
-    }).catch((err)=>{
-        console.log(err);
-        return res.status(500);
+const loginUser = async (req, res) => {
+  console.log("InTheFunction");
+  const userId = req.query.userId;
+  const password = req.query.password;
+  console.log(userId);
+  userData.findOne({ userId: userId, password: password });
+  console
+    .log("user:", user)
+    .then((user) => {
+      res.setHeader("Content-Type", "text/plain");
+      if (!user) {
+        return res.status(404).send("login failed");
+      }
+      const accessToken = jwt.sign(userId, process.env.ACCESS_TOKEN_SECRET);
+      console.log(accessToken);
+      res.json({ accessToken: accessToken });
+      return res.status(200).send(user);
     })
-}
-const usersRecipes = async(req, res) => {
-    const userId = req.query.userId;
-    userData.find( { userId: { $ne:  userId } } )
-    .then((users) =>{
-        console.log(users)
-        res.setHeader("Content-Type", "text/plain");
-        return res.status(200).send(users);
-    }).catch((err)=>{
-        console.log(err);
-        return res.status(500);
-    })
-}
+    .catch((err) => {
+      console.log(err);
+      return res.status(500);
+    });
+};
 
-module.exports={loginUser,usersRecipes};
+// const loginUser = (req, res) => {
+//   const userId = req.query.userId;
+//   const password = req.query.password;
+//   userData.findOne({ userId: userId, password: password }).then((res) => {
+//     if (res.data.accessToken) {
+//       localStorage.setItem("user", JSON.stringify(res.data));
+//     }
+//     return res.data;
+//   });
+// };
+const usersRecipes = async (req, res) => {
+  const userId = req.query.userId;
+  userData
+    .find({ userId: { $ne: userId } })
+    .then((users) => {
+      console.log(users);
+      res.setHeader("Content-Type", "text/plain");
+      return res.status(200).send(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500);
+    });
+};
+
+module.exports = { loginUser, usersRecipes };
